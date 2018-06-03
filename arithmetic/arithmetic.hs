d4989e7daa96f6c6a27c28806260b2430bb4d8a1
kobori92 <kobori.tomoyuki@gmail.com>
2018-06-03 22:35:01 +0900

    型なし算術式作成

--- arithmetic/arithmetic.hs
+++ arithmetic/arithmetic.hs
@@ -1,8 +1,42 @@
+
 data Term =
-  | true
-  | false
-  | if of Term * Term * Term
-  | zero
-  | succ of Term
-  | pred of Term
-  | isZero of Term
+    True
+  | False
+  | If Term Term Term
+  | Zero
+  | Succ Term
+  | Pred Term
+  | IsZero Term
+
+isNumericValue t = case t of
+  Zero   -> Prelude.True
+  Succ n -> isNumericValue n
+  _      -> Prelude.False
+
+isValue t = case t of
+  Main.True  -> Prelude.True
+  Main.False -> Prelude.True
+  _          -> isNumericValue t
+
+evaluate1step t = case t of
+  (If Main.True thn _)  -> thn
+  (If Main.False _ els) -> els
+  (If cond thm els)     -> (If cond' thm els)
+    where cond' = evaluate1step cond
+  (Succ n)              -> Succ n'
+    where n' = evaluate1step n
+  (Pred Zero)           -> Zero
+  (Pred t)              -> Pred t'
+    where t' = evaluate1step t
+  (IsZero Zero)         -> Main.True
+  (IsZero (Succ _))     -> Main.False
+  (IsZero t)            -> IsZero t'
+    where t' = evaluate1step t
+  _ -> t
+
+evaluate t
+  | isValue t  = t
+  | Prelude.True =  evaluate t'
+      where t' = evaluate1step t
+
+
