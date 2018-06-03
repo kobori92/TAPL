f23c485ac9abac8ff18232810056f2e8d08dac95
kobori92 <kobori.tomoyuki@gmail.com>
2018-06-03 23:06:36 +0900

    TermをShowのインスタンスに追加

--- arithmetic/arithmetic.hs
+++ arithmetic/arithmetic.hs
@@ -1,39 +1,49 @@
 
 data Term =
-    True
-  | False
+    Tru
+  | Fals
   | If Term Term Term
   | Zero
   | Succ Term
   | Pred Term
   | IsZero Term
 
+instance Show Term where
+  show Tru               = "Tru"
+  show Fals              = "Fals"
+  show (If cond thn els) =
+    "If (" ++ (show cond) ++ ") then (" ++ (show thn) ++ ") else (" ++ (show els) ++")"
+  show Zero              = "Zero"
+  show (Succ n)          = "Succ (" ++ (show n) ++ ")"
+  show (Pred t)          = "Pred (" ++ (show t) ++ ")"
+  show (IsZero n)        = "IsZero (" ++ (show n) ++ ")"
+
 isNumericValue t = case t of
-  Zero   -> Prelude.True
+  Zero   -> True
   Succ n -> isNumericValue n
-  _      -> Prelude.False
+  _      -> False
 
 isValue t = case t of
-  Main.True  -> Prelude.True
-  Main.False -> Prelude.True
-  _          -> isNumericValue t
+  Tru  -> True
+  Fals -> True
+  _    -> isNumericValue t
 
 evaluate1step t = case t of
-  (If Main.True thn _)  -> thn
-  (If Main.False _ els) -> els
-  (If cond thm els)     -> (If cond' thm els)
+  (If Tru thn _)    -> thn
+  (If Fals _ els)   -> els
+  (If cond thm els) -> (If cond' thm els)
     where cond' = evaluate1step cond
-  (Succ n)              -> Succ n'
+  (Succ n)          -> Succ n'
     where n' = evaluate1step n
-  (Pred Zero)           -> Zero
-  (Pred (Succ n))       -> n
-  (Pred t)              -> Pred t'
+  (Pred Zero)       -> Zero
+  (Pred (Succ n))   -> n
+  (Pred t)          -> Pred t'
     where t' = evaluate1step t
-  (IsZero Zero)         -> Main.True
-  (IsZero (Succ _))     -> Main.False
-  (IsZero t)            -> IsZero t'
+  (IsZero Zero)     -> Tru
+  (IsZero (Succ _)) -> Fals
+  (IsZero t)        -> IsZero t'
     where t' = evaluate1step t
-  _ -> t
+  _                 -> t
 
 evaluate t
   | isValue t  = t
