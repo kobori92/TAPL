0a24cd1741d7c44eb8f5b093eaf5193795619929
kobori92 <kobori.tomoyuki@gmail.com>
2018-06-06 23:40:18 +0900

    シフト関数作成（ちゃんと動かない）

--- lambda/lambda.hs
+++ lambda/lambda.hs
@@ -25,5 +25,17 @@ showTerm context term = case term of
 instance Show Term where
     show t = showTerm [] t
 
+shiftTerm :: Term -> Int -> Term
+shiftTerm term distance = shift term distance 0
+    where shift term distance cut = case term of
+	Var index     -> if index < cut
+                             then Var index
+                             else Var (index + distance)
+        Abs name body -> Abs name shiftedBody
+                             where shiftedBody = shift body distance (cut + 1)
+        App func arg  -> App shiftedFunc shiftedArg
+                             where shiftedFunc = shift func distance cut;
+                                   shiftedArg = shift arg distance cut
+
 x = Var 0
 abs = Abs "x" x
