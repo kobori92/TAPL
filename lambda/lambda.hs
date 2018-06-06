9120b3ebb061a1d908961bd66978fc78e136fa37
Kobori Tomonori <koboritomonori@N-20171214-075.local>
2018-06-06 20:05:20 +0900

    Term をShowのインスタンスに

--- lambda/lambda.hs
+++ lambda/lambda.hs
@@ -8,20 +8,20 @@ pickFreshName ctx x
     | otherwise = pickFreshName ctx (x ++ "'")
 
 showTerm ctx t = case t of
-    Var i     -> ctx !! i
+    Var i     -> if (length ctx <= i)
+                    then "x" ++ show i
+                    else ctx !! i
     Abs x t   -> "(l" ++ x' ++ ". " ++ showTerm ctx' t ++ ")"
                  where
                      (ctx', x') = pickFreshName ctx x
     App t1 t2 -> "(" ++ showTerm ctx t1 ++ " " ++ showTerm ctx t2 ++ ")"
 
-{-
 instance Show Term where
-  show (Var _) = "x"
+    show t = showTerm [] t
+    {-
+  show (Var i) = showTerm "x"
   show (Abs x t) = "\\" ++ "x. " ++ show t
   show (App t1 t2) = "(" ++ show t1 ++ ") (" ++ show t2 ++ ")"
-
-nameVariable n = n + 1
-
+-}
 x = Var 0
 abs = Abs "x" x
--}
