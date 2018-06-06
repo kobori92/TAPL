8c880f2bf0877c4b542722a86097ae63d374232d
kobori92 <kobori.tomoyuki@gmail.com>
2018-06-06 23:25:15 +0900

    型シノニムの利用 変数名変更 関数の型を明記

--- lambda/lambda.hs
+++ lambda/lambda.hs
@@ -1,27 +1,29 @@
+type BindingName = String
+
 data Term =
       Var Int
-    | Abs String Term
+    | Abs BindingName Term
     | App Term Term
 
-pickFreshName ctx x
-    | not (x `elem` ctx) = (x : ctx, x)
-    | otherwise = pickFreshName ctx (x ++ "'")
+type Context = [BindingName]
 
-showTerm ctx t = case t of
-    Var i     -> if (length ctx <= i)
-                    then "x" ++ show i
-                    else ctx !! i
-    Abs x t   -> "(l" ++ x' ++ ". " ++ showTerm ctx' t ++ ")"
-                 where
-                     (ctx', x') = pickFreshName ctx x
-    App t1 t2 -> "(" ++ showTerm ctx t1 ++ " " ++ showTerm ctx t2 ++ ")"
+pickFreshName :: Context -> BindingName -> (Context, BindingName)
+pickFreshName context name
+    | not (name `elem` context) = (name : context, name)
+    | otherwise = pickFreshName context (name ++ "'")
+
+showTerm :: Context -> Term -> String
+showTerm context term = case term of
+    Var index     -> if (length context <= index)
+                         then "x" ++ show index
+                         else context !! index
+    Abs name body -> "(l" ++ freshName ++ ". " ++ showTerm newContext body ++ ")"
+                         where
+                           (newContext, freshName) = pickFreshName context name
+    App func arg  -> "(" ++ showTerm context func ++ " " ++ showTerm context arg ++ ")"
 
 instance Show Term where
     show t = showTerm [] t
-    {-
-  show (Var i) = showTerm "x"
-  show (Abs x t) = "\\" ++ "x. " ++ show t
-  show (App t1 t2) = "(" ++ show t1 ++ ") (" ++ show t2 ++ ")"
--}
+
 x = Var 0
 abs = Abs "x" x
